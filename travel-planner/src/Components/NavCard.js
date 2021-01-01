b985bd7c3f041bec7632638cff5fb75b3a67f1da
Maclyn Afonso <>
2021-01-01 02:17:01 +0000

    created nav bars for each category

---
+++ travel-planner/src/Components/NavCard.js
@@ -0,0 +1,23 @@
+import { Card, CardContent, Typography } from '@material-ui/core'
+import React from 'react'
+import './NavCard.css'
+
+function NavCard({ title, countPlaces, countCountries }) {
+    return (
+        <Card className='navCard'>
+            <CardContent>
+                <Typography className='navCard__header'>
+                    {title}
+                </Typography>
+                <Typography>
+                    Places: {countPlaces}
+                </Typography>
+                <Typography>
+                    Countries: {countCountries}
+                </Typography>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default NavCard
